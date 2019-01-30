77108c27872323b7f133ee1f0ada5107b1dc07c6
ItsDizzy <itsdizzy40@gmail.com>
2019-01-30 10:27:00 +0100

    Use correct project type this time

---
+++ Practicum 1 Deel A/main.c
@@ -0,0 +1,18 @@
+/*
+ * Practicum 1 Deel A.c
+ *
+ * Created: 1/30/2019 10:26:24 AM
+ * Author : paulh
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
