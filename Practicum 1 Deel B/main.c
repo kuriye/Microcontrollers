a74cace10994b69daaafe3ce56f6d9ed06e400a6
Boudewijn <boudewijn348@gmail.com>
2019-01-30 13:02:05 +0100

    b5 yaay

--- Practicum 1 Deel B/main.c
+++ Practicum 1 Deel B/main.c
@@ -3,6 +3,7 @@
 
 #include <avr/io.h>
 #include <util/delay.h>
+#include "pattern.h"
 
 void b2() {
 	DDRD = 0xFF;
@@ -45,9 +46,47 @@ void b4() {
 	}
 }
 
+void wait( int ms )
+{
+	for (int i=0; i<ms; i++)
+	{
+		_delay_ms( 1 );		// library function (max 30 ms at 8MHz)
+	}
+}
+
+void b5() {
+	PATTERN_STRUCT pattern[] = {
+		{0x00, 100}, {0x01, 100}, {0x02, 100}, {0x04, 100}, {0x08, 100}, {0x10, 100}, {0x20, 100}, {0x40, 100}, {0x80, 100},
+		{0x80, 100}, {0x40, 100}, {0x20, 100}, {0x10, 100}, {0x08, 100}, {0x04, 100}, {0x02, 100}, {0x01, 100}, {0x00, 100},
+		{0x00, 100},
+		{0x81, 100}, {0x42, 100}, {0x24, 100}, {0x18, 100}, {0x0F, 200}, {0xF0, 200}, {0x0F, 200}, {0xF0, 200},
+		{0x00, 0}
+	};
+	
+	DDRA = 0xFF;	
+	DDRB = 0xFF;	
+	DDRC = 0xFF;	
+	DDRD = 0xFF;					
+	
+	while (1)
+	{
+
+		for(int i = 0; i < (sizeof(pattern)/sizeof(pattern[0])); i++) {
+			PORTA = pattern[i].data;
+			PORTB = pattern[i].data;
+			PORTC = pattern[i].data;
+			PORTD = pattern[i].data;
+		
+			wait(pattern[i].delay);
+		}
+	}
+}
+
+
+
 int main( void )
 {
-	b4();
+	b5();
 
 	return 1;
 }
---
+++ Practicum 1 Deel B/pattern.h
@@ -0,0 +1,20 @@
+/*
+ * pattern.h
+ *
+ * Created: 30/01/2019 12:46:03
+ *  Author: Bou's Laptop
+ */ 
+
+
+#ifndef PATTERN_H_
+#define PATTERN_H_
+
+typedef struct {
+	unsigned char data;
+	unsigned int delay ;
+} PATTERN_STRUCT;
+
+
+
+
+#endif /* PATTERN_H_ */
