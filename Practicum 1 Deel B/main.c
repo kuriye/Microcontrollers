dbb886647c6b3586bf968bea7c0e05ea2f97a1b8
ItsDizzy <itsdizzy40@gmail.com>
2019-01-30 12:10:39 +0100

    B.3 also donez

--- Practicum 1 Deel B/main.c
+++ Practicum 1 Deel B/main.c
@@ -1,41 +1,38 @@
-/* ---------------------------------------------------------------------------
-** This software is in the public domain, furnished "as is", without technical
-** support, and with no warranty, express or implied, as to its usefulness for
-** any purpose.
-**
-** knipper.c
-**
-** Beschrijving:	Toggle even en oneven leds PORTD  
-** Target:			AVR mcu
-** Build:			avr-gcc -std=c99 -Wall -O3 -mmcu=atmega128 -D F_CPU=8000000UL -c knipper.c
-**					avr-gcc -g -mmcu=atmega128 -o knipper.elf knipper.o
-**					avr-objcopy -O ihex knipper.elf knipper.hex 
-**					or type 'make'
-** Author: 			dkroeske@gmail.com
-** -------------------------------------------------------------------------*/
-
 #define F_CPU 8000000L
-#define PIN(x) (1 << x)
+#define PORT(x) (1 << x)
 
 #include <avr/io.h>
 #include <util/delay.h>
 
 void b2() {
+	DDRD = 0xFF;
+	
 	while (1)
 	{
-		PORTD = PIN(7);
+		PORTD = PORT(7);
 		_delay_ms(500);
-		PORTD = PIN(6);
+		PORTD = PORT(6);
 		_delay_ms(500);
+	}
+
+}
+
+void b3() {
+	PORTC = 0x10;
+	DDRD = 0xFF;
+	
+	while(1) {
+		if(PINC & 0b0000001) {
+			PORTD = PORT(7);
+			} else {
+			PORTD = 0x00;
+		}
 	}
 }
 
 int main( void )
 {
-	
-	DDRD = 0xFF;			// All pins PORTD are set to output 
-	
-	b2();
+	b3();
 
 	return 1;
 }
