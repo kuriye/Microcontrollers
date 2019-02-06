9b342f7079cc54f49f7602cd2e9434f450caff3a
ItsDizzy <itsdizzy40@gmail.com>
2019-02-06 11:40:33 +0100

    Interruptzz

--- Practicum 2/main.c
+++ Practicum 2/main.c
@@ -10,76 +10,43 @@
 #include <util/delay.h>
 #include <avr/interrupt.h>
 
-
-/******************************************************************/
-void wait( int ms )
-/* 
-short:			Busy wait number of millisecs
-inputs:			int ms (Number of millisecs to busy wait)
-outputs:	
-notes:			Busy wait, not very accurate. Make sure (external)
-				clock value is set. This is used by _delay_ms inside
-				util/delay.h
-Version :    	DMK, Initial code
-*******************************************************************/
-{
-	for (int i=0; i<ms; i++)
-	{
-		_delay_ms( 1 );		// library function (max 30 ms at 8MHz)
-	}
-}
-
-/******************************************************************/
-ISR( INT0_vect )
-/* 
-short:			ISR INT0
-inputs:			
-outputs:	
-notes:			Set PORTD.5
-Version :    	DMK, Initial code
-*******************************************************************/
-{
-    PORTD |= (1<<5);		
-}
-
-/******************************************************************/
 ISR( INT1_vect )
-/* 
-short:			ISR INT1
-inputs:			
-outputs:	
-notes:			Clear PORTD.5
-Version :    	DMK, Initial code
-*******************************************************************/
 {
-    PORTD &= ~(1<<5);		
+	if(PORTA >= 0x80) {
+		PORTA = 0x01;
+	} else {
+		PORTA = PORTA << 1;
+	}
+	_delay_ms(100);
 }
 
-/******************************************************************/
+ISR( INT2_vect )
+{
+	if(PORTA > 0x01) {
+		PORTA = PORTA >> 1;
+	} else {
+		PORTA = 0x80;
+	}
+	_delay_ms(100);
+}
+
 int main( void )
-/* 
-short:			main() loop, entry point of executable
-inputs:			
-outputs:	
-notes:			Slow background task after init ISR
-Version :    	DMK, Initial code
-*******************************************************************/
 {
 	// Init I/O
-	DDRD = 0xF0;			// PORTD(7:4) output, PORTD(3:0) input	
+	DDRA = 0xFF;
+	DDRD = 0xF0;			// PORTD(3:0) input
 
 	// Init Interrupt hardware
-	EICRA |= 0x0B;			// INT1 falling edge, INT0 rising edge
-	EIMSK |= 0x03;			// Enable INT1 & INT0
+	EICRA |= 0x3C;			// 0b0011 1100
+	EIMSK |= 0x06;			// 0b0000 0110
 	
 	// Enable global interrupt system
-	//SREG = 0x80;			// Of direct via SREG of via wrapper
 	sei();				
 
-	while (1)
-	{
-		PORTD ^= (1<<7);	// Toggle PORTD.7
-		wait( 500 );								
+	PORTA = 0x01;
+
+	while (1) {
+		
 	}
 
 	return 1;
