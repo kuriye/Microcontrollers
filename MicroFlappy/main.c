8d1d064174273c875fef2f003a5eea430cc96479
paulhobbel <itsdizzy40@gmail.com>
2019-03-28 14:01:32 +0100

    Auto stash before merge of "develop" and "origin/develop"

--- MicroFlappy/main.c
+++ MicroFlappy/main.c
@@ -9,7 +9,8 @@
 
 #include <stdio.h>
 #include <avr/io.h>
-#include <avr/delay.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
 
 #include "drivers/rtc.h"
 #include "drivers/lcd.h"
@@ -22,23 +23,42 @@
 #include "logic/delay.h"
 #include "logic/sound.h"
 
-int main(void)
+#define TRIGGER_PIN (1 << 0)
+#define ECHO_PIN (1 << 4)
+
+int overflow = 0;
+
+ISR(TIMER3_OVF_vect)
 {
-	I2CInit();
-	TimeInit();
-	RtcInit();
-	//LcdInit();
-	matrix_init();
-	SoundInit();
-	Ultrasonicinit();
+	overflow++;
+}
+
+int main(void)
+{	
+	DDRE = 0x01;
+	PORTE = 0xFF;
 	
+	//DDRA = 0x01;		/* Make trigger pin as output */
+	//PORTD = 0xFF;
+	
+	sei();					/* Enable global interrupt */
+	ETIMSK = (1 << TOIE3);	/* Enable Timer3 overflow interrupts */
+	TCCR3A = 0;				/* Set all bit to zero Normal operation */
+	
+	
+	I2CInit();
+	
+	//SoundInit();
+	
+	//LcdInit();
+	MatrixInit();
 	MatrixClear();
-	LcdInit();
+	
 	SoundTune song[10];
 	uint16_t i;
 	for(i = 0; i < 10; i++) //create default song
 	{
-		song[i] = (SoundTune) {.duration_us = 1000000, .freq_Hz = 250 * (i+1)};
+		//song[i] = (SoundTune) {.duration_us = 1000000, .freq_Hz = 250 * (i+1)};
 	}
 	
 	//SoundSetUpdateMusic((SoundMusic) {.tunes = song, .tunesAmount = 10});
@@ -46,10 +66,34 @@ int main(void)
 	
     while (1)
     {
-		MatrixFill();
-		Delay_ms(1000);
 		MatrixClear();
-		Delay_ms(1000);
+		PORTE |= TRIGGER_PIN;
+		_delay_us(10);
+		PORTE &= ~TRIGGER_PIN;
+		
+		/* Clear Timer Counter, 
+		start timer on rising edge with no prescaler, 
+		clear input capture flag and 
+		clear overflow flag. */
+		TCNT3 = 0; 
+		TCCR3B |= ((1 << ICES3) | (1 << CS30)); 
+		ETIFR = 1 << ICF3; 
+		//ETIFR = 1 << TOV3;
+	
+		/* Wait for rising edge */
+		while((ETIFR & (1 << ICF3)) == 0);
+		TCNT3 = 0;
+		TCCR3B = (1 << CS30);
+		ETIFR = 1 << ICF3;
+		//ETIFR = 1 << TOV3;
+		overflow = 0;
+		
+		MatrixFill();
+		//_delay_ms(2000);
+		//MatrixFill();
+		//Delay_ms(1000);
+		//MatrixClear();
+		//Delay_ms(1000);
 		
 		//SoundUpdate();
     }
